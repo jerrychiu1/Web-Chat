63478d78de1a684c6c4c2251aa71f75ef8c03d6d
jerrychiu1 <JerryChiu1@hotmail.com>
2018-03-04 20:54:21 -0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
         
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Jerry',
-        text: 'Hello, anyone?'
-    });
 });
 
 socket.on('disconnect', function () {
