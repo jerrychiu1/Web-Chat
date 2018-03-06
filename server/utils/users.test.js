54e0ab9154223f64e51d96a1fe692eb8eff4f333
jerrychiu1 <JerryChiu1@hotmail.com>
2018-03-05 22:28:26 -0800

    Add Users class and updateUserList event

---
+++ server/utils/users.test.js
@@ -0,0 +1,78 @@
+const expect = require('expect');
+
+const {Users} = require('./users');
+
+describe('Users', () => {
+    var users;
+
+    beforeEach(() => {
+        users = new Users();
+        users.users = [{
+            id: '1',
+            name: 'Jerry',
+            room: 'Test'
+        }, {
+            id: '2',
+            name: 'Jen',
+            room: 'Room'
+        }, {
+            id: '3',
+            name: 'Mike',
+            room: 'Test'
+        }];
+    });
+
+    it('should add new user', () => {
+        var users = new Users();
+        var user = {
+            id: '123',
+            name: 'Jerry',
+            room: 'Test'
+        };
+        var resUser = users.addUser(user.id, user.name, user.room);
+
+        expect(users.users).toEqual([user]);
+    });
+
+    it('should remove a user', () => {
+        var userId = '1';
+        var user = users.removeUser(userId);
+
+        expect(user.id).toBe(userId);
+        expect(users.users.length).toBe(2);
+    });
+
+    it('should not remove user', () => {
+        var userId = '99';
+        var user = users.removeUser(userId);
+
+        expect(user).toBeFalsy();
+        expect(users.users.length).toBe(3);
+    });
+
+    it('should find user', () => {
+        var userId = '2';
+        var user = users.getUser(userId);
+
+        expect(user.id).toBe(userId);
+    });
+
+    it('should not find user', () => {
+        var userId = '99';
+        var user = users.getUser(userId);
+        
+        expect(user).toBeFalsy();
+    });
+
+    it('should return names for test room', () => {
+        var userList = users.getUserList('Test');
+
+        expect(userList).toEqual(['Jerry', 'Mike']);
+    });
+
+    it('should return names for room room', () => {
+        var userList = users.getUserList('Room');
+
+        expect(userList).toEqual(['Jen']);
+    });
+})
